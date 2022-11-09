2fbb242d6c43c36338738ecc99eb1fb51d6535e0
ilya-Varfolomeev <ilya.varfolomeev@fusion-tech.pro>
2022-11-09 16:38:27 +0300

    Make TodoFilters

---
+++ src/components/TodoFilters.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  emits: ["filter"],
+  methods: {
+    handleFilter(settings) {
+      console.log("123", settings);
+      this.$emit("filter", settings);
+    },
+  },
+};
+</script>
+
+<template>
+  <button @click="handleFilter('all')" class="todo-filters__button">All</button>
+  <button @click="handleFilter('completed')" class="todo-filters__button">
+    Completed
+  </button>
+  <button @click="handleFilter('left')" class="todo-filters__button">
+    Left
+  </button>
+</template>
+
+<style scoped>
+.todo-filters__button {
+  border: 1px solid #ececec;
+  border-radius: 5px;
+  justify-self: end;
+  background: white;
+  padding: 10px;
+  cursor: pointer;
+}
+</style>
