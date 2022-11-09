1ef5899d64897596dcb0e5320e8e200cf1b46c28
ilya-Varfolomeev <ilya.varfolomeev@fusion-tech.pro>
2022-11-09 14:02:07 +0300

    Implement TodoList with logic

---
+++ src/components/TodoList.vue
@@ -0,0 +1,83 @@
+<script>
+import TodoItem from "./TodoItem.vue";
+
+export default {
+  components: { TodoItem },
+  data() {
+    return {
+      newTodoText: "",
+      todos: localStorage.getItem("todos")
+        ? JSON.parse(localStorage.getItem("todos"))
+        : [],
+    };
+  },
+  methods: {
+    changeTodosArr(todosArr) {
+      this.todos = todosArr;
+      localStorage.setItem("todos", JSON.stringify(todosArr));
+    },
+    handleAddTodo() {
+      if (this.newTodoText.length < 1) {
+        return;
+      }
+
+      const newTodo = {
+        title: this.newTodoText,
+        isDone: false,
+        id: Math.floor(Math.random() * 100),
+      };
+      const allTodos = [...this.todos, newTodo];
+      this.changeTodosArr(allTodos);
+      this.newTodoText = "";
+    },
+    handleRemoveTodo(todoId) {
+      const filteredTodos = this.todos.filter((todo) => todo.id !== todoId);
+      this.changeTodosArr(filteredTodos);
+    },
+    handleFilterTodos(filter) {
+      switch (filter) {
+        case "all":
+          return (this.todos = JSON.parse(localStorage.getItem("todos")));
+        case "completed":
+          return (this.todos = JSON.parse(localStorage.getItem("todos")).filter(
+            (todo) => todo.isDone
+          ));
+        case "left":
+          return (this.todos = JSON.parse(localStorage.getItem("todos")).filter(
+            (todo) => !todo.isDone
+          ));
+      }
+    },
+    handleChangeTodoStatus(todoId, value) {
+      const filteredTodos = this.todos.map((todo) => {
+        if (todo.id === todoId) {
+          return { ...todo, isDone: value };
+        }
+        return todo;
+      });
+      this.changeTodosArr(filteredTodos);
+    },
+  },
+};
+</script>
+<template>
+  <form v-on:submit.prevent="handleAddTodo">
+    <input type="text" placeholder="Enter a new todo" v-model="newTodoText" />
+    <button>Add</button>
+  </form>
+  <ul>
+    <TodoItem
+      v-for="todo in todos"
+      :key="todo.id"
+      :title="todo.title"
+      :checked="todo.isDone"
+      @remove="handleRemoveTodo(todo.id)"
+      @check="handleChangeTodoStatus(todo.id, !todo.isDone)"
+    />
+  </ul>
+  <template v-if="this.todos.length > 0">
+    <button @click="handleFilterTodos('all')">All</button>
+    <button @click="handleFilterTodos('completed')">Completed</button>
+    <button @click="handleFilterTodos('left')">Left</button>
+  </template>
+</template>
