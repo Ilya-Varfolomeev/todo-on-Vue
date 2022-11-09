da565181f396d51f023956c72868d4397ed56776
ilya-Varfolomeev <ilya.varfolomeev@fusion-tech.pro>
2022-11-09 14:02:15 +0300

    Implement TodoItem with logic

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  props: ["title", "checked"],
+  emits: ["remove", "check"],
+  data() {
+    return {
+      isChecked: this.checked,
+    };
+  },
+};
+</script>
+
+<template>
+  <li>
+    {{ title }}
+    <button @click="$emit('remove')">Remove</button>
+    <input
+      type="checkbox"
+      id="checkbox"
+      @click="$emit('check')"
+      v-model="this.isChecked"
+    />
+  </li>
+</template>
